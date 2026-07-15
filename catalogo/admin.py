a093f4374159aa470000caf53120a8d281d959a9
Rodrigo Castillo <rodrigo.castillo@uisek.edu.ec>
2026-07-14 22:51:04 -0500

    feat: configurar administracion de autores y libros

--- catalogo/admin.py
+++ catalogo/admin.py
@@ -1,3 +1,62 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Autor, Libro
+
+
+@admin.register(Autor)
+class AutorAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "nombre",
+        "apellido",
+        "nacionalidad",
+        "activo",
+        "creado_en",
+    )
+    search_fields = (
+        "nombre",
+        "apellido",
+        "nacionalidad",
+    )
+    list_filter = (
+        "activo",
+        "nacionalidad",
+    )
+    ordering = (
+        "apellido",
+        "nombre",
+    )
+    readonly_fields = (
+        "creado_en",
+        "actualizado_en",
+    )
+
+
+@admin.register(Libro)
+class LibroAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "titulo",
+        "autor",
+        "isbn",
+        "genero",
+        "idioma",
+        "disponible",
+    )
+    search_fields = (
+        "titulo",
+        "isbn",
+        "autor__nombre",
+        "autor__apellido",
+    )
+    list_filter = (
+        "genero",
+        "idioma",
+        "disponible",
+    )
+    ordering = ("titulo",)
+    readonly_fields = (
+        "creado_en",
+        "actualizado_en",
+    )
+    autocomplete_fields = ("autor",)
